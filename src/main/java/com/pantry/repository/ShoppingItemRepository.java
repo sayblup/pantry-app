89a68e63055293055ad8d6fd32309543d281fec1
sayblup <sayblup@gmail.com>
2026-05-10 17:39:48 +0200

    Add files via upload

---
+++ src/main/java/com/pantry/repository/ShoppingItemRepository.java
@@ -0,0 +1,9 @@
+package com.pantry.repository;
+
+import com.pantry.model.ShoppingItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+}
